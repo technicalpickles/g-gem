0500834bfff692a2be2d95b785fd876ee3b2e9b2
Josh Nichols <josh@technicalpickles.com>
2008-05-20 00:01:38 -0400

    Split out looking up data from writing out ebuild. Now will lookup/write dependencies as well.

--- g-gem.rb
+++ g-gem.rb
@@ -8,11 +8,6 @@ require 'erubis'
 
 package = ARGV.first || 'activerecord'
 
-inst = Gem::DependencyInstaller.new
-specs = inst.find_spec_by_name_and_version(package)
-
-
-
 class Ebuild
   attr_accessor :spec, :source
   
@@ -35,7 +30,7 @@ class Ebuild
   def write
     output = eruby.evaluate(self)
     FileUtils.mkdir_p('ebuilds')
-    File.open(filename, 'w') {|f| f.write(output) }
+    File.open("ebuilds/#{filename}", 'w') {|f| f.write(output) }
   end
   
   protected
@@ -48,10 +43,36 @@ class Ebuild
     @eruby
   end
   
+  def self.create_from_spec_lookup(package)
+    @@inst ||= Gem::DependencyInstaller.new
+    spec_pair = @@inst.find_spec_by_name_and_version(package).first
+    self.new(spec_pair)
+  end
 end
 
-specs.each do |spec_pair|
-  ebuild = Ebuild.new(spec_pair)
+
+gems_to_lookup = [package]
+ebuilds = {}
+until gems_to_lookup.empty?
+  next_package = gems_to_lookup.shift
   
+  unless ebuilds.has_key?(next_package)
+    puts "Gathering info about #{next_package}..."
+  
+    ebuilds[next_package] = Ebuild.create_from_spec_lookup(next_package)
+  else
+    puts "Already know about #{next_package}"
+  end
+  
+  ebuilds[next_package].spec.dependencies.each do |dependency|
+    unless ebuilds.has_key? dependency.name
+      puts "Need to lookup dependency #{dependency.name}"
+      gems_to_lookup.push(dependency.name)
+    end
+  end
+end
+
+ebuilds.each_pair do |name, ebuild|
+  puts "Writing out #{ebuild.filename}"
   ebuild.write
 end
